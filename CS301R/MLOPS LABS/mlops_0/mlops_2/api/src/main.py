f788af59b678023a5beaf1ce5dbe4e4fbef5a427
WebDevNovice <jacobgbullock3@gmail.com>
2026-02-05 10:29:10 -0700

    adding the api service

--- CS301R/MLOPS LABS/mlops_0/mlops_2/api/src/main.py
+++ CS301R/MLOPS LABS/mlops_0/mlops_2/api/src/main.py
@@ -4,6 +4,12 @@ from fastapi.responses import HTMLResponse
 app = FastAPI(title="MLOps Service")
 
 # TODO 1: Create GET /health that returns JSON like {"status": "ok"}
+@app.get('/health')
+def health():
+    return {'status':'ok'}
 
+@app.head('/health')
+def health_head():
+    return Response(status_code=status.HTTP_200_OK)
 
 # TODO 2: Create GET /hello that returns an HTML page using HTMLResponse
