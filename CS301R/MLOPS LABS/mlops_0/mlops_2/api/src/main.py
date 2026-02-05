7a689a44ab956147d708da5bbcbcc3a4d576363c
WebDevNovice <jacobgbullock3@gmail.com>
2026-02-05 10:40:55 -0700

    finished today's lab

--- CS301R/MLOPS LABS/mlops_0/mlops_2/api/src/main.py
+++ CS301R/MLOPS LABS/mlops_0/mlops_2/api/src/main.py
@@ -13,3 +13,15 @@ def health_head():
     return Response(status_code=status.HTTP_200_OK)
 
 # TODO 2: Create GET /hello that returns an HTML page using HTMLResponse
+@app.get("/hello", response_class=HTMLResponse)
+def hello():
+    return """<!doctype html>
+                <html>
+                <head><title>MLOps Lab 1</title></head>
+                    <body>
+                        <h1>Hello there!</h1>
+                        <h1>General Kenobi!! GUAHH *Cough* *Cough*</h1>
+                            <p>A legendary battle ensures</p>
+                    </body>
+                </html>
+"""
